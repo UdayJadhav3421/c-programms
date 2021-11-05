2a071f62486220db7192fe2032ff1e2866afee1a
Uj3421 <93025782+Uj3421@users.noreply.github.com>
2021-11-05 21:27:18 +0530

    Add files via upload

---
+++ Basic programms/ To print Student Information Database.cxx
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    printf("\n\t Student Information Database\n");
+
+    printf("\n\n============******============");
+
+    printf("\n %-4d %-10s %3d %3d %3d %3d %0.2f",120,"Gaurav",6,95,65,166,55.3333);
+    printf("\n %-4d %-10s %3d %3d %3d %3d %0.2f",6,"Abhi",55,45,80,180,60.0000);
+    printf("\n %-4d %-10s %3d %3d %3d %3d %0.2f",95,"Uday",65,85,50,200,66.6666);
+
+    printf("\n\n============******============");
+    
+    getch();
+    return 0;
+}
