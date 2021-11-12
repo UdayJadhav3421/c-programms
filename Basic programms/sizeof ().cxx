155767572b9523e114c1c94e0e8ebba0030790dd
Uj3421 <93025782+Uj3421@users.noreply.github.com>
2021-11-12 17:10:12 +0530

    Add files via upload

---
+++ Basic programms/sizeof ().cxx
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int no = 55;
+	char my = 'F';
+	double dnum = 47562.22;
+
+	printf("\n \t size 1= %d", sizeof(no));
+
+	printf("\n \t size 2= %d", sizeof(dnum));
+
+	printf("\n \t size 3= %d", sizeof(my));
+
+	printf("\n \t size 4= %d", sizeof(long int));
+
+	printf("\n \t size 5= %d", sizeof(float));
+
+	printf("\n \t size 6= %d", sizeof(char));
+
+	return 0;
+}
