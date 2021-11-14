cda7f11933cfe91112a2c15569ace8ebed0d7233
Uday Jadhav 3421 <93025782+Uj3421@users.noreply.github.com>
2021-11-14 21:56:50 +0530

    Add files via upload

---
+++ Pattern Printing/Pattern - Left Lower Triangular in Stars.cxx
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int i = 0, j = 0, x = 0;
+
+	printf("\n Enter a Number=");
+	scanf("%d", &x);
+
+	for (i = 1; i <= x; i++)
+	{
+		for (j = 1; j <= x; j++)
+		{
+			if (i >= j)
+			{
+				printf(" * ");
+			}
+			else
+			{
+				printf("   ");
+			}
+		}
+		printf("\n");
+	}
+	getch();
+	return 0;
+}
