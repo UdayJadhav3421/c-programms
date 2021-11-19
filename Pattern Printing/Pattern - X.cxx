d6b3fff3d40d0020be36b1a8ee90e1c910294e29
Uday Jadhav 3421 <93025782+Uj3421@users.noreply.github.com>
2021-11-19 18:04:58 +0530

    Add files via upload

---
+++ Pattern Printing/Pattern - X.cxx
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int i, j, x;
+	printf("\n Enter a Number=");
+	scanf("%d", &x);
+
+	printf("\n\n Pattern Printing is=>\n\n");
+
+	for (i = 1; i <= x; i++)
+	{
+		for (j = 1; j <= x; j++)
+		{
+			if (i == j || i + j == x + 1)
+			{
+				printf(" * ");
+			}
+			else
+			{
+				printf("   ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
