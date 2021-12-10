a589b0a91d84a08cebc485643e7af5a134a1c591
UdayJadhav3421 <93025782+UdayJadhav3421@users.noreply.github.com>
2021-12-10 20:34:29 +0530

    Add files via upload

---
+++ Basic programms/02 Using putch & putchar.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char Name = getchar();
+
+    putch('\n');
+    putch('\n');
+    putch('ABC');
+    putch('\n');
+
+    getche();
+
+    putchar('\t');
+    putchar('Name');
+    putchar('\n');
+    putchar('H');
+
+    getche();
+
+    getch();
+    return 0;
+}
