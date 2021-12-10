145051237dd37f3032f919d75dbd2a1d34ac8e56
UdayJadhav3421 <93025782+UdayJadhav3421@users.noreply.github.com>
2021-12-10 20:33:59 +0530

    Add files via upload

---
+++ Basic programms/01 Using putch & putchar.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch = 'A', Grade = 'B', Group = 'U';
+
+    putch('\n');
+    putchar('\t');
+
+    printf("Welcome To putch/puchar Demonstration =>\n\n");
+
+    putch(ch);
+    putchar('\n');
+
+    putchar(ch);
+    putch('\n');
+
+    putch(Group);
+    putch('\n');
+
+    putchar('M');
+    putch('\n');
+
+    putch('India');
+    putch('\n');
+
+    putch('Grade');
+    putch('\n');
+
+    getch();
+    return 0;
+}
