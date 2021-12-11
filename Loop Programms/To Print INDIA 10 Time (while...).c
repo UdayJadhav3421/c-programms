7829abc8e1c0b06f4b9acbc8da726a8e0dacf5a4
UdayJadhav3421 <93025782+UdayJadhav3421@users.noreply.github.com>
2021-12-11 18:30:00 +0530

    Add files via upload

---
+++ Loop Programms/To Print INDIA 10 Time (while...).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int cnt = 1;
+    printf("\n\n Printing Name 10 Time=>");
+
+    while(cnt<=10)
+    {
+        printf("\n\t\t INDIA");
+        cnt++;
+    }
+    getch();
+    return 0;
+}
