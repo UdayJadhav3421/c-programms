e96f6ac0bf55c434ca04b7b0b11db2f27e45175f
UdayJadhav3421 <93025782+UdayJadhav3421@users.noreply.github.com>
2021-12-28 21:06:40 +0530

    Add files via upload

---
+++ Loop Programms/To Find Out Factorial of Given Number - Using for loop-(Fancy).cxx
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int No = 0, temp = 0, Fact = 1;
+	printf("\n Enter a Number=");
+	scanf("%d", &No);
+
+	for (temp = No, Fact = 1; temp > 0; Fact *= temp, temp--);
+
+		printf("\n Factorial of %d is = %d", No, Fact);
+
+	getch();
+	return 0;
+}
