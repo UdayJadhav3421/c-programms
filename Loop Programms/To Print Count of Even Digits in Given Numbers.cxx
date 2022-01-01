b01f530b2fda1d11ac5878b72a2ae0a4c777dcb2
UdayJadhav3421 <93025782+UdayJadhav3421@users.noreply.github.com>
2022-01-01 23:25:58 +0530

    Add files via upload

---
+++ Loop Programms/To Print Count of Even Digits in Given Numbers.cxx
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int No = 0, Dig = 0, Even_Count = 0, temp = 0;
+	printf("\n Enter a Number=");
+	scanf("%d", &No);
+
+	temp = No;
+	
+	while (temp > 0)
+	{
+		Dig = temp % 10;
+		
+		if ((Dig !=0) && (Dig % 2 == 0))
+		{
+			Even_Count++;
+		}
+		temp = temp / 10;
+	}
+	printf("\n Even Digit in %d are = %d", No, Even_Count);
+
+	getch();
+	return 0;
+}
