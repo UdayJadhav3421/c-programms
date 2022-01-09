8ffcab034ac9707848f0020e014e058e17a9f46f
UdayJadhav3421 <93025782+UdayJadhav3421@users.noreply.github.com>
2022-01-09 21:55:25 +0530

    Update and rename Count Numbers of Digits in Given Number .cxx to Count Numbers of Digits in Given Number .c

--- Loop Programms/Count Numbers of Digits in Given Number .c
+++ Loop Programms/Count Numbers of Digits in Given Number .c
@@ -3,15 +3,18 @@
 int main()
 {
     int No=0,dcnt=0,temp=0;
+    
     printf("\n Enter a Number=");
     scanf("%d",&No);
     
     temp=No;
+    
     while(temp>0)
     {
         temp=temp/10;
         dcnt++;
     }
+    
     printf("\n Digit in %d are = %d",No,dcnt);
     
     getch();
